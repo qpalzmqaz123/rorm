51b0207cb752b1b7fdc41dfb1c6b0368b809a952
wangqj <qpalzmqaz123@gmail.com>
2021-12-24 13:45:42 +0800

    support define table_name

--- rorm-macro/src/parser.rs
+++ rorm-macro/src/parser.rs
@@ -1,6 +1,6 @@
 use proc_macro_error::abort;
 use quote::ToTokens;
-use syn::{Attribute, Data, DataStruct, DeriveInput, Expr, Meta};
+use syn::{Attribute, Data, DataStruct, DeriveInput, Expr, Lit, Meta};
 
 #[derive(Debug, Clone)]
 pub struct ColumnInfo {
@@ -19,6 +19,7 @@ pub struct TableInfo {
 
 #[derive(Debug)]
 enum AttrInfo {
+    TableName(String),
     PrimaryKey,
 }
 
@@ -29,8 +30,28 @@ pub fn parse(input: DeriveInput) -> TableInfo {
     };
 
     let struct_name = input.ident.to_string();
-    let table_name = struct_name.clone();
+    let mut table_name = struct_name.clone();
     let model_name = format!("{}Model", table_name);
+
+    // Parse struct attrs
+    for attr in &input.attrs {
+        let attr_name = attr.path.to_token_stream().to_string();
+        if attr_name != "rorm" {
+            abort!(attr, "Attr name must be 'rorm'")
+        }
+
+        let attr_infos = parse_rorm_attr(attr);
+
+        // Process attr
+        for attr_info in &attr_infos {
+            match &attr_info {
+                AttrInfo::TableName(name) => table_name = name.clone(),
+                _ => abort!(attr, "Invalid struct attr field: {:?}", attr_info),
+            }
+        }
+    }
+
+    // Parse columns
     let (columns, primary_keys) = parse_columns(st);
 
     TableInfo {
@@ -62,12 +83,13 @@ fn parse_columns(st: &DataStruct) -> (Vec<ColumnInfo>, Vec<String>) {
                 abort!(attr, "Attr name must be 'rorm'")
             }
 
-            let attrs = parse_rorm_attr(attr);
+            let attr_infos = parse_rorm_attr(attr);
 
             // Process attr
-            for attr in attrs {
-                match attr {
+            for attr_info in attr_infos {
+                match attr_info {
                     AttrInfo::PrimaryKey => primary_keys.push(name.clone()),
+                    _ => abort!(attr, "Invalid column attr field: {:?}", attr_info),
                 }
             }
         }
@@ -82,7 +104,7 @@ fn parse_columns(st: &DataStruct) -> (Vec<ColumnInfo>, Vec<String>) {
 fn parse_rorm_attr(attr: &Attribute) -> Vec<AttrInfo> {
     const PARSE_ERR_STR: &'static str =
         "Parse to metalist failed, syntax is #[rorm(field [= value])]";
-    const ARG_HELP: &'static str = "Syntax is rorm([primary_key])";
+    const ARG_HELP: &'static str = r#"Syntax is rorm([primary_key | table_name = "NAME"])"#;
 
     let mut attrs = Vec::<AttrInfo>::new();
 
@@ -108,12 +130,35 @@ fn parse_rorm_attr(attr: &Attribute) -> Vec<AttrInfo> {
                 match field_name.as_str() {
                     // Parse primary_key
                     "primary_key" => attrs.push(AttrInfo::PrimaryKey),
-                    _ => abort!(expr, "Syntax error"; help = ARG_HELP),
+
+                    // Error
+                    _ => abort!(expr, "Syntax error while decode path"; help = ARG_HELP),
                 }
             }
-            _ => abort!(expr, "Syntax error"; help = ARG_HELP),
+            Expr::Assign(assign) => {
+                let field_name = assign.left.to_token_stream().to_string();
+                match field_name.as_str() {
+                    // Parse table_name = "NAME"
+                    "table_name" => attrs.push(AttrInfo::TableName(get_str(&assign.right))),
+
+                    // Error
+                    _ => abort!(expr, "Syntax error while decode assign"; help = ARG_HELP),
+                }
+            }
+            _ => abort!(expr, "Syntax error while metch expr"; help = ARG_HELP),
         }
     }
 
     attrs
+}
+
+/// Get string from expr
+fn get_str(expr: &Expr) -> String {
+    if let Expr::Lit(lit) = expr {
+        if let Lit::Str(s) = &lit.lit {
+            return s.value();
+        }
+    }
+
+    abort!(expr, "Expect string")
 }
